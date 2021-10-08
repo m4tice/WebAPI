7463f2bbb6f45a8213ca01858dc55726f4dac461
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-08 16:42:42 +0200

    Final commit

--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Data;
 using WebAPI.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WebAPI.Controllers
 {
@@ -17,10 +19,12 @@ namespace WebAPI.Controllers
 	public class EmployeeController : ControllerBase
 	{
 		private readonly IConfiguration _configuration;
+		private readonly IWebHostEnvironment _env;
 
-		public EmployeeController(IConfiguration configuration)
+		public EmployeeController(IConfiguration configuration, IWebHostEnvironment env)
 		{
 			_configuration = configuration;
+			_env = env;
 		}
 
 		[HttpGet]
@@ -145,6 +149,59 @@ namespace WebAPI.Controllers
 			}
 
 			return new JsonResult("Delete Successfully");
+		}
+
+		[Route("SaveFile")]
+		[HttpPost]
+		public JsonResult SaveFile()
+		{
+			try
+			{
+				var httpRequest = Request.Form;
+				var postedFile = httpRequest.Files[0];
+				string filename = postedFile.FileName;
+				var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+				using (var stream = new FileStream(physicalPath, FileMode.Create))
+				{
+					postedFile.CopyTo(stream);
+				}
+
+				return new JsonResult(filename);
+			}
+
+			catch (Exception)
+			{
+				return new JsonResult("anonymous.png");
+			}
+		}
+
+		[Route("GetAllDepartmentNames")]
+		public JsonResult GettAllDepartmentNames()
+		{
+			string query = @"
+					select DepartmentName from dbo.Department
+					";
+
+			DataTable table = new DataTable();
+			string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+			SqlDataReader myReader;
+			using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+			{
+				myCon.Open();
+				using (SqlCommand myCommand = new SqlCommand(query, myCon))
+				{
+					myReader = myCommand.ExecuteReader();
+					table.Load(myReader);
+
+
+					myReader.Close();
+					myCon.Close();
+
+				}
+			}
+
+			return new JsonResult(table);
 		}
 	}
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,6 +11,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Serialization;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace WebAPI
 {
@@ -58,6 +60,14 @@ namespace WebAPI
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
+			});
+
+			app.UseStaticFiles(new StaticFileOptions
+			{
+				FileProvider = new PhysicalFileProvider(
+					Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+				RequestPath="/Photos"
+
 			});
 		}
 	}
